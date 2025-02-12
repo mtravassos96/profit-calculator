8c8a3a581a34ccbed126efea8a54b286aca87913
Matheus Travassos <mtravassos96@gmail.com>
2025-02-12 13:56:21 -0300

    Update profit_calculator.go
    - fixed the profit margin ratio
    - feat functions for getting user input and outputing calculations

--- profit_calculator.go
+++ profit_calculator.go
@@ -5,8 +5,22 @@ import (
 )
 
 func main() {
-	var revenue, expenses, taxRate float64
+	revenue, expenses, taxRate := getUserInput()
 
+	earningsBeforeTax, profit, ratio := calculateFinancials(revenue, expenses, taxRate)
+
+	formattedEBT := fmt.Sprintf("Earnings Before Tax: USD %.2f\n", earningsBeforeTax)
+	formattedProfit := fmt.Sprintf("Earnings After Tax: USD %.2f\n", profit)
+	formattedRatio := fmt.Sprintf("Ratio: %.2f\n", ratio)
+
+	fmt.Print(formattedEBT, formattedProfit, formattedRatio)
+
+	// fmt.Printf("Earnings Before Tax: USD %.2f\nEarnings After Tax: USD %.2f\nRatio: %.2f\n", earningsBeforeTax, profit, ratio)
+}
+
+func getUserInput() (float64, float64, float64) {
+	var revenue, expenses, taxRate float64
+	
 	fmt.Print("Inform your revenue (USD): ")
 	fmt.Scan(&revenue)
 
@@ -16,15 +30,13 @@ func main() {
 	fmt.Print("Inform the tax rate: ")
 	fmt.Scan(&taxRate)
 
+	return revenue, expenses, taxRate
+}
+
+func calculateFinancials(revenue, expenses, taxRate float64) (float64, float64, float64) {
 	earningsBeforeTax := revenue - expenses
 	profit := earningsBeforeTax * (1 - taxRate/100)
-	ratio := earningsBeforeTax / profit
+	ratio :=  profit / earningsBeforeTax
 
-	formattedEBT := fmt.Sprintf("Earnings Before Tax: USD %.2f\n", earningsBeforeTax)
-	formattedProfit := fmt.Sprintf("Earnings After Tax: USD %.2f\n", profit)
-	formattedRatio := fmt.Sprintf("Ratio: %.2f\n", ratio)
-
-	fmt.Print(formattedEBT, formattedProfit, formattedRatio)
-
-	// fmt.Printf("Earnings Before Tax: USD %.2f\nEarnings After Tax: USD %.2f\nRatio: %.2f\n", earningsBeforeTax, profit, ratio)
+	return earningsBeforeTax, profit, ratio
 }
