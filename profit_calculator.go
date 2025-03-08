dc86a47c4baa2e5aea6f9bba392483c5c5eb0ea9
Matheus Travassos <mtravassos96@gmail.com>
2025-03-07 23:17:41 -0300

    update profit_calculator.go
    - import "os" and "errors"
    - feat function to store the calculations to a .txt file
    - validation of user input
    - error handling

--- profit_calculator.go
+++ profit_calculator.go
@@ -2,10 +2,25 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"errors"
 )
 
+// Goals
+// 1) Validate user input
+//    - Show error message & exit if invalid input is provided
+//    - No negative numbers
+//    - Not 0
+// 2) Store calculated results into file
+
+const userFinancialsFile = "financials.txt"
+
 func main() {
-	revenue, expenses, taxRate := getUserInput()
+	revenue, expenses, taxRate, err := getUserInput()
+	if err != nil {
+		fmt.Println("ERROR:", err)
+		return
+	}
 
 	earningsBeforeTax, profit, ratio := calculateFinancials(revenue, expenses, taxRate)
 
@@ -15,10 +30,12 @@ func main() {
 
 	fmt.Print(formattedEBT, formattedProfit, formattedRatio)
 
+	writeFinancialsToFile(formattedEBT, formattedProfit, formattedRatio)
+
 	// fmt.Printf("Earnings Before Tax: USD %.2f\nEarnings After Tax: USD %.2f\nRatio: %.2f\n", earningsBeforeTax, profit, ratio)
 }
 
-func getUserInput() (float64, float64, float64) {
+func getUserInput() (float64, float64, float64, error) {
 	var revenue, expenses, taxRate float64
 	
 	fmt.Print("Inform your revenue (USD): ")
@@ -30,7 +47,11 @@ func getUserInput() (float64, float64, float64) {
 	fmt.Print("Inform the tax rate: ")
 	fmt.Scan(&taxRate)
 
-	return revenue, expenses, taxRate
+	if revenue <= 0 || expenses < 0 || taxRate <= 0 {
+		return 0, 0, 0, errors.New("input values must be a positive number")
+	}
+
+	return revenue, expenses, taxRate, nil
 }
 
 func calculateFinancials(revenue, expenses, taxRate float64) (float64, float64, float64) {
@@ -39,4 +60,9 @@ func calculateFinancials(revenue, expenses, taxRate float64) (float64, float64, 
 	ratio :=  profit / earningsBeforeTax
 
 	return earningsBeforeTax, profit, ratio
+}
+
+func writeFinancialsToFile(formattedEBT, formattedProfit, formattedRatio string) {
+	combineTexts := formattedEBT + formattedProfit + formattedRatio
+	os.WriteFile(userFinancialsFile, []byte(combineTexts), 0644)
 }
